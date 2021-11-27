2aced51d0cb518f0487307e6c98aae50c3f1f797
bboydfresh <69738088+bboydfresh@users.noreply.github.com>
2021-11-27 18:45:10 -0500

    New Projects

---
+++ Sound Board/script.js
@@ -0,0 +1,25 @@
+const sounds = ['Gravity', 'Neon', 'New Light', 'Slow Dancing', 'Something Like Olivia', 'Stop This Train', 'Last Train Home']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
